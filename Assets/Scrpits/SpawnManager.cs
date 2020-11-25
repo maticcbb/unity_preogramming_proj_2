dd5b6395954391be5d94bb74493bb06eb9ee9088
Mateusz Mielcarz <m.mielcarz93@gmail.com>
2020-11-25 16:53:23 +0100

    Randomly generate animal spawn position

--- Assets/Scrpits/SpawnManager.cs
+++ Assets/Scrpits/SpawnManager.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +19,9 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),0,spawnPosZ);
             int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20),
+            Instantiate(animalPrefabs[animalIndex], spawnPosition,
                 animalPrefabs[animalIndex].transform.rotation);
         }
     }
